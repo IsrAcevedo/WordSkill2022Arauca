331b5c782a5705d1caffaa1801e56fc4d0428055
IsrAcevedo <112662280+IsrAcevedo@users.noreply.github.com>
2022-11-29 15:22:35 -0500

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function cambiarFondo1(){
+    document.getElementById('contenedorImg').classList.remove('contenedorFondo2');
+    document.getElementById('contenedorImg').classList.add('contenedorFondo1');
+   
+}
+
+function cambiarFondo2(){
+    document.getElementById('contenedorImg').classList.remove('contenedorFondo1');
+    document.getElementById('contenedorImg').classList.add('contenedorFondo2');
+   
+}
+
